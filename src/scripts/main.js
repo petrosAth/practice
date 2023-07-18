1a004e54a997f626e6c7adcbf97599b79cc65c33
petrosAth <develop@athanasoulis.me>
2023-07-18 20:31:37 +0300

    Add fetchGit()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,11 @@ import '../stylesheets/normalize.css';
 import '../stylesheets/typography.css';
 import '../stylesheets/variables.css';
 import '../stylesheets/main.css';
+
+const fetchGif = () =>
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=wTpxTeZdtT4DgfYZ7tZcyZyJnqHQN2kc&s=cat', {
+    mode: 'cors',
+  })
+    .then((response) => response.json())
+    .then((response) => response.data.images.original.url)
+    .catch((error) => console.log(error));
