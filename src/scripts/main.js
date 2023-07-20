0aca32d5316d378d9e7ebf186ee1da7caf08556c
petrosAth <develop@athanasoulis.me>
2023-07-20 16:25:10 +0300

    Add renderImg()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,23 @@ import '../stylesheets/typography.css';
 import '../stylesheets/variables.css';
 import '../stylesheets/main.css';
 
-const fetchGif = () =>
+const renderImg = (url) => {
+  const img = document.querySelector('#img');
+  img.src = url;
+};
+
+const loadImg = () => {
   fetch('https://api.giphy.com/v1/gifs/translate?api_key=wTpxTeZdtT4DgfYZ7tZcyZyJnqHQN2kc&s=cat', {
     mode: 'cors',
   })
     .then((response) => response.json())
-    .then((response) => response.data.images.original.url)
+    .then((response) => {
+      renderImg(response.data.images.original.url);
+    })
     .catch((error) => console.log(error));
+};
+const initApp = () => {
+  loadImg();
+};
+
+initApp();
