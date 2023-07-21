8004e246423d5f47e66cf9524e21ae9a86a94e76
petrosAth <develop@athanasoulis.me>
2023-07-21 13:38:00 +0300

    Use async/await instead of promise/then

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,30 +10,32 @@ const renderImg = (url) => {
 
 const getInput = () => {
   const input = document.querySelector('#input');
-  return input.value || 'cat';
+  return input.value;
 };
 
-const getGif = (searchString) => {
-  return new Promise((resolve, reject) => {
-    fetch(
+const getGif = async (searchString) => {
+  try {
+    const data = await fetch(
       `https://api.giphy.com/v1/gifs/translate?api_key=wTpxTeZdtT4DgfYZ7tZcyZyJnqHQN2kc&s=${searchString}&rating=r`,
       { mode: 'cors' }
-    )
-      .then((response) => {
-        return response.status == 200 ? response.json() : reject(`\nERROR: getGif | status: ${response.status}`);
-      })
-      .then((response) => resolve(response.data.images.original.url))
-      .catch((error) => console.log(`\nERROR: getGif | ${error}`));
-  });
+    );
+    const dataJson = await data.json();
+    const gifUrl = dataJson.data.images.original.url;
+
+    return gifUrl;
+  } catch (err) {
+    console.log(`ERROR: on function ${getGif.name} | ${err}`);
+  }
 };
 
-const showGif = () => {
-  const searchString = getInput();
-  getGif(searchString)
-    .then(renderImg)
-    .catch((error) => {
-      console.log(`\nERROR: showGif | ${error}`);
-    });
+const showGif = async () => {
+  try {
+    const searchString = getInput() || 'cat';
+    const gifUrl = await getGif(searchString);
+    renderImg(gifUrl);
+  } catch (err) {
+    console.log(`ERROR: on function ${showGif.name} | ${err}`);
+  }
 };
 
 const btnListener = (func) => {
