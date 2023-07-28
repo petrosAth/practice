cde49fa733a6739e2d72f73ca36a8741f61b00af
petrosAth <develop@athanasoulis.me>
2023-07-28 10:31:36 +0300

    Add size function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,32 +14,61 @@ class LinkedList {
   constructor() {
     this.listHead = null;
   }
+
   append(value) {
     const node = this.listHead;
+
     if (node === null) {
       this.listHead = new Node(value);
       return;
-    } else {
-      const searchList = (node) => {
-        if (node.nextNode === null) {
-          node.nextNode = new Node(value);
-          return;
-        }
-        searchList(node.nextNode);
-      };
-      return searchList(node);
     }
+
+    const searchList = (node) => {
+      if (node.nextNode === null) {
+        node.nextNode = new Node(value);
+        return;
+      }
+
+      searchList(node.nextNode);
+    };
+
+    return searchList(node);
   }
+
   prepend(value) {
     let node = this.listHead;
+
     if (node === null) {
       node = new Node(value);
       return;
     }
+
     const newHeadNode = new Node(value, node);
     this.listHead = newHeadNode;
   }
+
   get head() {
     return this.listHead;
+  }
+
+  get size() {
+    const node = this.listHead;
+    let size = 0;
+
+    if (node === null) {
+      return size;
+    }
+
+    const searchList = (node) => {
+      size++;
+
+      if (node.nextNode !== null) {
+        searchList(node.nextNode);
+      }
+
+      return size;
+    };
+
+    return searchList(node);
   }
 }
