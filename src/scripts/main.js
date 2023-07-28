6a890d7b47086554c6cbad0ebe8699efc5f3bc7b
petrosAth <develop@athanasoulis.me>
2023-07-28 09:56:03 +0300

    Add append function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,35 @@ import '../stylesheets/normalize.css';
 import '../stylesheets/typography.css';
 import '../stylesheets/variables.css';
 import '../stylesheets/main.css';
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.listHead = null;
+  }
+  append(value) {
+    const node = this.listHead;
+    if (node === null) {
+      this.listHead = new Node(value);
+      return;
+    } else {
+      const searchList = (node) => {
+        if (node.nextNode === null) {
+          node.nextNode = new Node(value);
+          return;
+        }
+        searchList(node.nextNode);
+      };
+      return searchList(node);
+    }
+  }
+  get head() {
+    return this.listHead;
+  }
+}
